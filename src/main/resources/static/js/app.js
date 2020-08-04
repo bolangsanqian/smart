7a145433d55aa734207908340b9f5b9136af10ca
liwei <liwei@comtom.cn>
2020-08-04 12:26:24 +0800

    修改样式

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -17,29 +17,37 @@ var vm = new Vue({
     methods: {
     },
     created: function () {
-        var sn = urlParam("sn");
-        mui.ajax('/detail/' + sn,{
-            dataType:'json',//服务器返回json格式数据
-            type:'get',//HTTP请求类型
-            timeout:10000,//超时时间设置为10秒；
-            headers:{'Content-Type':'application/json'},
-            success:function(rs){
-                console.log(rs);
-                if (rs.code == 200) {
-                    vm.detail = rs.data;
-                    vm.detail.temperature = vm.detail.temperature ? vm.detail.temperature : {};
-                    vm.detail.humidity = vm.detail.humidity ? vm.detail.humidity : {};
-                    vm.detail.illumination = vm.detail.illumination ? vm.detail.illumination : {};
-                    vm.detail.co2 = vm.detail.co2 ? vm.detail.co2 : {};
-                } else {
-                    mui.alert(rs.msg);
-                }
-            },
-            error:function(xhr,type,errorThrown){
-                console.log(type);
-            }
-        });
+        loadData();
     }
 })
+
+function loadData() {
+    var sn = urlParam("sn");
+    mui.ajax('/detail/' + sn,{
+        dataType:'json',//服务器返回json格式数据
+        type:'get',//HTTP请求类型
+        timeout:10000,//超时时间设置为10秒；
+        headers:{'Content-Type':'application/json'},
+        success:function(rs){
+            console.log(rs);
+            if (rs.code == 200) {
+                vm.detail = rs.data;
+                vm.detail.temperature = vm.detail.temperature ? vm.detail.temperature : {};
+                vm.detail.humidity = vm.detail.humidity ? vm.detail.humidity : {};
+                vm.detail.illumination = vm.detail.illumination ? vm.detail.illumination : {};
+                vm.detail.co2 = vm.detail.co2 ? vm.detail.co2 : {};
+            } else {
+                console.error("设备不存在，SN: " + sn);
+            }
+        },
+        error:function(xhr,type,errorThrown){
+            console.log(type);
+        }
+    });
+}
+
+setInterval(function () {
+    loadData();
+}, 2000)
 
 
